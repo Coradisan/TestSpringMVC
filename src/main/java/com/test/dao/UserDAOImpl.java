56f99629984e2c3bf26ceb77c354999a44933315
Coradisan <289724407@qq.com>
2017-08-08 23:45:32 +0800

    QueryRunner

--- src/main/java/com/test/dao/UserDAOImpl.java
+++ src/main/java/com/test/dao/UserDAOImpl.java
@@ -1,11 +1,24 @@
 package com.test.dao;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.test.db.DBUtils;
+import com.test.entity.User;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.*;
+import org.junit.Test;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Admin on 2017/8/4 0004.
@@ -15,6 +28,7 @@ public class UserDAOImpl {
 
     @Autowired
     private JdbcTemplate template;
+    private Connection connection = null;
 
 //    public JdbcTemplate getTemplate() {
 //        return template;
@@ -34,4 +48,24 @@ public class UserDAOImpl {
 //        System.out.println(template);
 //        template.queryForList("SELECT * FROM user");
 //    }
+
+
+    @Test
+    public void getListT() {
+
+        QueryRunner run = new QueryRunner();
+        connection = DBUtils.getConnection();
+        ResultSetHandler<List<User>> h = new BeanListHandler<>(User.class);
+        try {
+//            Map<String, Object> resultMap = run.query(connection, "select * from user", new MapHandler());
+            List<User> arr = run.query(connection, "select * from user", h);
+
+            System.out.println(arr.toString());
+
+        } catch (SQLException sqle) {
+            // Handle it
+        }
+    }
+
+
 }
